f128e0b7db685be6236131ee385ffb63040c5e34
Mariusz <mariusz.janowski@yahoo.pl>
2022-07-27 21:37:06 +0200

    active class in slider

--- script.mjs
+++ script.mjs
@@ -2,21 +2,40 @@ import myJson from "./data.json" assert { type: "json" };
 
 const imageContainer = document.querySelector(".image-container");
 
-function sliderCreator() {
-  myJson.map((el, index) => {
-    const slider = document.createElement("img");
-    slider.setAttribute("src", `${el.url}`);
-    slider.setAttribute("alt", `${el.name}`);
-    slider.setAttribute("id", `${el.name}`);
-    slider.setAttribute("z-index", `${index}`);
-    slider.classList.add("image");
-    return imageContainer.appendChild(slider);
+// Images Creator
+// function createSlider() {
+//   myJson.map((el) => {
+//     const slider = document.createElement("img");
+//     slider.setAttribute("src", `${el.url}`);
+//     slider.setAttribute("alt", `${el.name}`);
+//     slider.setAttribute("id", `${el.name}`);
+
+//     slider.classList.add("image");
+//     return imageContainer.appendChild(slider);
+//   });
+// }
+// createSlider();
+
+// Photo selector
+function changePhoto() {
+  const btns = document.querySelectorAll("button");
+  const sliders = document.querySelectorAll("img");
+
+  // for (let i = 0; i < btns.length; i += 1) {
+  //   btns[i].addEventListener("click", () => {
+  //     const selectedSlider = document.getElementsByClassName("image")[i];
+  //     selectedSlider.classList.add("active");
+  //     const current = document.getElementsByClassName("active");
+  //     current[0].className = current[0].className.replace(" active", "");
+  //   });
+  // }
+
+  btns.forEach((button) => {
+    button.addEventListener("click", function () {
+      sliders.forEach((slider) => slider.classList.remove("active"));
+      this.classList.add("active");
+    });
   });
 }
-sliderCreator();
 
-const btns = document.getElementsByClassName("dot");
-
-for (let i = 0; i < btns.length; i += 1) {
-  btns[i].addEventListener("click", () => {});
-}
+changePhoto();
