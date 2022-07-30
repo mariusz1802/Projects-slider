bd32f4160281f9fffb6a56889b3e95af56f62615
Mariusz <mariusz.janowski@yahoo.pl>
2022-07-30 09:40:14 +0200

    fadeOut Image

--- script.mjs
+++ script.mjs
@@ -1,34 +1,42 @@
-import myJson from './data.json' assert { type: 'json' };
+import myJson from "./data.json" assert { type: "json" };
 
-const imageContainer = document.querySelector('.image-container');
+const imageContainer = document.querySelector(".image-container");
 
-// Images Creator
+// // Images Creator
 function createSlider() {
-  myJson.map((el) => {
-    const slider = document.createElement('img');
-    slider.setAttribute('src', `${el.url}`);
-    slider.setAttribute('alt', `${el.name}`);
-    slider.setAttribute('id', `${el.name}`);
+  myJson.map((el, index) => {
+    const slider = document.createElement("img");
+    slider.setAttribute("src", `${el.url}`);
+    slider.setAttribute("alt", `${el.name}`);
+    slider.setAttribute("id", `${el.name}`);
+    slider.classList.add("image");
 
-    slider.classList.add('image');
+    if (index == 0) {
+      slider.classList.add("active");
+    }
     return imageContainer.appendChild(slider);
   });
 }
 createSlider();
-
 // Photo selector
 function changePhoto() {
-  const btns = document.querySelectorAll('span');
-  const sliders = document.querySelectorAll('img');
+  const btns = document.querySelectorAll("span");
+  const sliders = document.querySelectorAll("img");
 
   for (let i = 0; i < btns.length; i += 1) {
-    btns[i].addEventListener('click', () => {
-      const selectedSlider = document.getElementsByClassName('image')[i];
-      const selectedButton = document.getElementsByClassName('dot')[i];
-      sliders.forEach((slider) => slider.classList.remove('active'));
-      btns.forEach((btn) => btn.classList.remove('active'));
-      selectedSlider.classList.add('active');
-      selectedButton.classList.add('active');
+    btns[i].addEventListener("click", () => {
+      const selectedSlider = document.getElementsByClassName("image")[i];
+      const selectedButton = document.getElementsByClassName("dot")[i];
+      const previousSlider = document.getElementsByClassName("image")[i];
+
+      sliders.forEach((slider) => slider.classList.remove("active"));
+      btns.forEach((btn) => btn.classList.remove("active"));
+      selectedSlider.classList.add("active");
+      selectedSlider.classList.add("previous");
+      if (document.querySelectorAll(".previous").length > 1) {
+        console.log("dwie");
+      }
+      selectedButton.classList.add("active");
     });
   }
 }
